aed3e2deacaf7333162021da77301066a4e3d2e2
Vladimir <kotopes231@gmail.com>
2024-04-10 20:41:49 +0300

    sort backend data

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -7,6 +7,7 @@ import {
   GridSortModel
 } from "@mui/x-data-grid-premium";
 import {useNavigate, useSearchParams} from "react-router-dom";
+import sortBy from 'sort-by';
 
 const columns: GridColDef[] = [
   { field: 'col1', headerName: 'Column 1', width: 150 },
@@ -17,6 +18,15 @@ enum SearchParamNames  {
   SortModel = 'sortModel',
   PaginationModel = 'paginationModel',
   FilterModel = 'filterModel'
+}
+
+const convertSortModalToBackendSort = (sortModel: GridSortModel) => {
+  if(sortModel.length > 0) {
+    const {sort, field } = sortModel[0];
+    const isDesc = sort === 'desc';
+    return `${isDesc ? '-': ''}${field}`
+  }
+  return undefined;
 }
 
 const createHeaderFilterSearchParamKeys = (field: string) => {
@@ -32,23 +42,36 @@ const rows: GridRowsProp = [
   { id: 4, col1: 'DataGridPro2', col2: 'is Awesome f' },
   { id: 5, col1: 'MUI2', col2: 'is Amazing f' },
 ];
-const fetchRows = (offset: number, limit: number) => {
+const fetchRows = (params: {
+  offset: number,
+  limit: number,
+  sort?: string
+}) => {
+  const {offset, limit, sort } = params;
+  console.log(params);
+
+  const sortedRows = [...rows];
+
+  if(sort) {
+    sortedRows.sort(sortBy(sort))
+  }
+
+  const paginatedRows = sortedRows.slice(offset, offset + limit);
+
   return {
     count: rows.length,
-    data: rows.slice(offset, offset + limit)
+    data: paginatedRows
   }
 }
 export const HomePage = () => {
 
-  const defaultPage = 0;
-  const defaultPageSize = 3;
   const modelsControlledByUrl = {
     ...useSortModelControlledByUrl({
       searchParamName: SearchParamNames.SortModel
     }),
     ...usePaginationModelControlledByUrl({
-      defaultPageSize: defaultPageSize,
-      defaultPage: defaultPage,
+      defaultPageSize: 3,
+      defaultPage: 0,
       searchParamName: SearchParamNames.PaginationModel
     }),
     ...useFilterModelControlledByUrl({
@@ -58,17 +81,21 @@ export const HomePage = () => {
 
   const {pageSize, page} = modelsControlledByUrl.paginationModel
 
-  const res = fetchRows(page * pageSize, pageSize)
+  const res = fetchRows({
+    offset: page * pageSize,
+    limit: pageSize,
+    sort: convertSortModalToBackendSort(modelsControlledByUrl.sortModel)
+  }, )
 
   return <DataGridPremium
     rowCount={res.count}
-    pageSizeOptions={[pageSize]}
     pagination
     paginationMode={'server'}
     filterMode={'server'}
     sortingMode={'server'}
     {...modelsControlledByUrl}
-    disableMultipleColumnsSorting={false}
+    disableMultipleColumnsSorting
+    disableMultipleColumnsFiltering
     headerFilters
     rows={res.data}
     columns={columns}
@@ -97,7 +124,7 @@ const useFilterModelControlledByUrl = ({
 
   const serializedFilterModel = searchParams.get(searchParamName)
 
-  const filterModel = serializedFilterModel ? JSON.parse(serializedFilterModel): {
+  const filterModel: GridFilterModel = serializedFilterModel ? JSON.parse(serializedFilterModel): {
     items: []
   }
 
@@ -127,7 +154,7 @@ const usePaginationModelControlledByUrl = ({
   };
 
   const serializedModel = searchParams.get(searchParamName);
-  const paginationModel = serializedModel ? JSON.parse(serializedModel): {
+  const paginationModel: GridPaginationModel = serializedModel ? JSON.parse(serializedModel): {
     page:  defaultPage,
     pageSize: defaultPageSize
   }
@@ -160,7 +187,7 @@ const useSortModelControlledByUrl = ({
 
   const serializedModel = searchParams.get(searchParamName);
 
-  const sortModel = serializedModel ? JSON.parse(serializedModel): []
+  const sortModel: GridSortModel = serializedModel ? JSON.parse(serializedModel): []
 
   return {
     onSortModelChange,
